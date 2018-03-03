46bd0534717585b27e1d8ef3a9dac17a7a85f140
AlexVicenteS <32463374+AlexVicenteS@users.noreply.github.com>
2018-03-03 13:23:47 +0100

    Update run_experiments.sh
    all 1st day experiments

--- OtherScripts/run_experiments.sh
+++ OtherScripts/run_experiments.sh
@@ -4,6 +4,8 @@
 
 # Baseline [TT100K dataset]
 
+# ---------- VGG ------------
+
 python train.py -c config/vgg_tt100k_classif.py -e vgg_default   
 
 # Substract mean and normalize by standard deviation (computed from training set) [TT100K dataset]
@@ -21,4 +23,22 @@ python train.py -c config/vgg_tt100k_classif_resize.py -e vgg_input_resize
 # Transfer learning to another dataset [TT100K dataset -> BelgiumTS dataset]
 
 python train.py -c config/vgg_belgiumTS_classif_transfer.py -e vgg_belgiumTS_classif_transfer
+
+# Baseline [KITTI dataset]
+
+python train.py -c config/vgg_kitti_classif_baseline.py -e vgg_tt100k_classif_baseline
+
+# Fine-tuning based on ImageNet weights [KITTI dataset]
+
+python train.py -c config/vgg_kitti_classif_finetune.py -e vgg_tt100k_classif_finetune
+
+# ---------- ResNet ------------
+
+# Baseline [TT100K dataset]
+
+python train.py -c config/resnet_tt100k_classif_baseline.py -e resnet_tt100k_classif_baseline
+
+# Fine-tuning based on ImageNet weights [TT100K dataset]
+
+python train.py -c config/resnet_tt100k_classif_finetune.py -e resnet_tt100k_classif_finetune
 
