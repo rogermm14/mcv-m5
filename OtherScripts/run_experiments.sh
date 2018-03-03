bec42302759d7d6441efc4c67ab2091a3a9b2f8f
AlexVicenteS <32463374+AlexVicenteS@users.noreply.github.com>
2018-03-03 12:24:38 +0100

    Update run_experiments.sh
    No more resize experiment lol

--- OtherScripts/run_experiments.sh
+++ OtherScripts/run_experiments.sh
@@ -12,11 +12,7 @@ python train.py -c config/tt100k_classif_preprocess.py -e vgg_preprocessing
 
 # Use random crops of 224x224 to feed the net [TT100K dataset]
 
-python train.py -c config/tt100k_classif_crops.py -e vgg_input_crops  
-
-# Use resized images of 224x224 to feed the net [TT100K dataset]
-
-python train.py -c config/tt100k_classif_resize.py -e vgg_input_resize  
+python train.py -c config/tt100k_classif_crops.py -e vgg_input_crops    
 
 # Transfer learning to another dataset [TT100K dataset]
 
