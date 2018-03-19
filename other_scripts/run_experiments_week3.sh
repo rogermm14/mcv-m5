ac3d32c3bf1a85a1bded7fb4dfcdd7f60c95eb22
rogermm14 <rogermarimolas@gmail.com>
2018-03-19 17:30:28 +0100

    Update run_experiments_week3.sh

--- other_scripts/run_experiments_week3.sh
+++ other_scripts/run_experiments_week3.sh
@@ -11,7 +11,7 @@ cd code
 
 #### Baseline code [TT100K dataset]
 
-#python train.py -c config/yolo_tt100k_detection.py -e yolo_tt100k_detection
+python train.py -c config/yolo_tt100k_detection.py -e yolo_tt100k_detection
 
 #### Baseline [Udacity dataset]
 
@@ -24,3 +24,32 @@ python train.py -c config/yolo_dataaug_tt100k_detection.py -e yolo_dataaug_tt100
 #### Data augmentation [Udacity dataset]
 
 python train.py -c config/yolo_dataaug_Udacity_detection.py -e yolo_dataaug_Udacity_detection
+
+# ---------- Faster R-CNN ------------ 
+
+#### Baseline code [TT100K dataset]
+
+cd ../other_scripts
+python create_gt_frcnn_TT100K_detection.py
+cd ../frcnn
+python train_frcnn.py -o simple -p ../other_scripts/TT100K_detection_train_labels.txt
+python test_frcnn.py -p ../Datasets/detection/TT100K_detection/test
+cd ../other_scripts
+python evaluate_frcnn_TT100K_detection.py
+
+
+rm -r ../frcnn/predictions
+rm ../frcnn/model_frcnn.hdf5
+rm ../frcnn/config.pickle
+mkdir ../frcnn/predictions
+
+
+#### Baseline code [Udacity dataset]
+
+python create_gt_frcnn_Udacity.py
+cd ../frcnn
+python train_frcnn.py -o simple -p ../other_scripts/Udacity_train_labels.txt
+python test_frcnn.py -p ../Datasets/detection/Udacity/test
+cd ../other_scripts
+python evaluate_frcnn_Udacity.py
+ 
